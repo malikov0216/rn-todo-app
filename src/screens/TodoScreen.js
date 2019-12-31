c716a70b6870f5ded6feaae25d04dfe810832247
Nartay <NDembayev@beeline.kz>
2019-12-31 12:55:41 +0600

    30 lesson 0:51

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import { THEME } from "../theme";
+import { AppCard } from "../components/ui/AppCard";
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title='Ред.' />
+      </AppCard>
       <View style={styles.buttons}>
-        <Button title='Назад' onPress={goBack} />
-        <Button
-          title='Удалить'
-          color='#ff0000'
-          onPress={() => console.log("To remove")}
-        />
+        <View style={styles.button}>
+          <Button title='Назад' onPress={goBack} color={THEME.GREY_COLOR} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Удалить'
+            color={THEME.DANGER_COLOR}
+            onPress={() => console.log("To remove")}
+          />
+        </View>
       </View>
     </View>
   );
@@ -21,5 +30,15 @@ const styles = StyleSheet.create({
   buttons: {
     flexDirection: "row",
     justifyContent: "space-between"
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
+  button: {
+    width: "40%"
+  },
+  title: {
+    fontSize: 20
   }
 });
